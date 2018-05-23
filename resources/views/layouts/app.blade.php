d46c6a138c510b919eccb5f970fb4fe377cc6e36
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-23 15:14:38 +0900

    git push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,6 @@
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="{{config('app.name', 'Laravel')}}">
     <meta property="og:locale" content="ja_JP">
-    <meta property="fb:app_id" content="339751459830292">
     <meta name="twitter:card" content="summary_large_image">
     <meta property="og:image" content="@yield('image',url('/thumbnail.jpg'))">
     <meta name="twitter:url" content="{{url()->current()}}">
@@ -184,75 +183,75 @@
                         </div>
                       </li>
                       @elseif($loop->iteration == 4)
-                          <li class="list-group-item">
-                           <div class="padding-wrap">
-                            <div class="col-xs-3 thumbnail">
-                              <a class="thumbnail-link" href="https://masakuraudo2.com/archives/2051">
-                                <img src="https://masakuraudo2.com/wp-content/uploads/2018/05/bra-2306600_640.jpg" alt="看護師・介護士のセクハラ被害の実態...。実際に体を触られた例も...">
-                              </a>
-                            </div>
-                            <div class="col-xs-9 title">
-                              <a class="title-link" href="https://masakuraudo2.com/archives/2051">
-                               看護師・介護士のセクハラ被害の実態...。実際に体を触られた例も...
-                             </a>
-                             <a class="description-link">
-                              @php
-                              echo mb_strimwidth("私も急に抱きつかれたことがあって。しかも夜中だったからめっちゃ怖かったです。。手のひらにキスされたのはほんと嫌でした。速攻手指消毒しました", 0, 120, '', 'utf8');
-                              @endphp
-                            </a>
-                          </div>
-                          <div class="clear"></div>
-                          <div class="col-xs-12 cat">
-                            <span class="cat-item">
-                              医療
-                            </span>
-                            <span class="cat-domain">
-                              @php
-                              echo parse_url("https://masakuraudo2.com/archives/2051", PHP_URL_HOST);
-                              @endphp
-                            </span>
-                            <a href="https://masakuraudo2.com/archives/2051" class="link-btn">サイトへ</a>
-                          </div>
-                          <span class="view">{{$article->view}} view</span>
-                        </div>
-                      </li>
-                      @endif
-                      <li class="list-group-item"  itemscope itemtype="http://schema.org/Article">
+                      <li class="list-group-item">
                        <div class="padding-wrap">
                         <div class="col-xs-3 thumbnail">
-                          <a class="thumbnail-link" href="{{$article->path()}}">
-                            <img src="{{$article->thumbnailPath()}}" alt="{{$article->title}}">
+                          <a class="thumbnail-link" href="https://masakuraudo2.com/archives/2051">
+                            <img src="https://masakuraudo2.com/wp-content/uploads/2018/05/bra-2306600_640.jpg" alt="看護師・介護士のセクハラ被害の実態...。実際に体を触られた例も...">
                           </a>
                         </div>
                         <div class="col-xs-9 title">
-                          <a class="title-link" href="{{$article->path()}}" itemprop="name">
-                           {{$article->title}}
+                          <a class="title-link" href="https://masakuraudo2.com/archives/2051">
+                           看護師・介護士のセクハラ被害の実態...。実際に体を触られた例も...
                          </a>
                          <a class="description-link">
                           @php
-                          echo mb_strimwidth($article->description, 0, 120, '', 'utf8');
+                          echo mb_strimwidth("私も急に抱きつかれたことがあって。しかも夜中だったからめっちゃ怖かったです。。手のひらにキスされたのはほんと嫌でした。速攻手指消毒しました", 0, 120, '', 'utf8');
                           @endphp
                         </a>
                       </div>
                       <div class="clear"></div>
                       <div class="col-xs-12 cat">
                         <span class="cat-item">
-                          {{$article->word->text}}
+                          医療
                         </span>
-                        <span class="cat-domain" itemprop="author">
+                        <span class="cat-domain">
                           @php
-                          echo parse_url($article->url, PHP_URL_HOST);
+                          echo parse_url("https://masakuraudo2.com/archives/2051", PHP_URL_HOST);
                           @endphp
                         </span>
-                        <a href="{{$article->path()}}" class="link-btn">サイトへ</a>
+                        <a href="https://masakuraudo2.com/archives/2051" class="link-btn">サイトへ</a>
                       </div>
                       <span class="view">{{$article->view}} view</span>
                     </div>
                   </li>
-                  @endforeach
+                  @endif
+                  <li class="list-group-item"  itemscope itemtype="http://schema.org/Article">
+                   <div class="padding-wrap">
+                    <div class="col-xs-3 thumbnail">
+                      <a class="thumbnail-link" href="{{$article->path()}}">
+                        <img src="{{$article->thumbnailPath()}}" alt="{{$article->title}}">
+                      </a>
+                    </div>
+                    <div class="col-xs-9 title">
+                      <a class="title-link" href="{{$article->path()}}" itemprop="name">
+                       {{$article->title}}
+                     </a>
+                     <a class="description-link">
+                      @php
+                      echo mb_strimwidth($article->description, 0, 120, '', 'utf8');
+                      @endphp
+                    </a>
+                  </div>
+                  <div class="clear"></div>
+                  <div class="col-xs-12 cat">
+                    <span class="cat-item">
+                      {{$article->word->text}}
+                    </span>
+                    <span class="cat-domain" itemprop="author">
+                      @php
+                      echo parse_url($article->url, PHP_URL_HOST);
+                      @endphp
+                    </span>
+                    <a href="{{$article->path()}}" class="link-btn">サイトへ</a>
+                  </div>
+                  <span class="view">{{$article->view}} view</span>
+                </div>
+              </li>
+              @endforeach
 
-                </ul>
-                <ul class="list-group">
+            </ul>
+            <ul class="list-group">
 
                       <!-- <div class="block">
                         <center>
@@ -337,48 +336,7 @@
                   <p style="margin:0;padding:16px 0;color:#aaa;text-align:right;font-size:.8rem;">© 2018 {{config('app.name')}}</p>
                 </footer>
 
-                <!-- Scripts -->
-                <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
-                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                <script>
-                  /* DOMの読み込み完了後に処理 */
-                  if(window.addEventListener) {
-                    window.addEventListener( "load" , shareButtonReadSyncer, false );
-                  }else{
-                    window.attachEvent( "onload", shareButtonReadSyncer );
-                  }
 
-                  /* シェアボタンを読み込む関数 */
-                  function shareButtonReadSyncer(){
-                    LineIt.loadButton();
-// Facebook
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-// Google+
-var scriptTag = document.createElement("script");
-scriptTag.type = "text/javascript"
-scriptTag.src = "https://apis.google.com/js/platform.js";
-scriptTag.async = true;
-document.getElementsByTagName("head")[0].appendChild(scriptTag);
-
-// はてなブックマーク
-var scriptTag = document.createElement("script");
-scriptTag.type = "text/javascript"
-scriptTag.src = "https://b.st-hatena.com/js/bookmark_button.js";
-scriptTag.async = true;
-document.getElementsByTagName("head")[0].appendChild(scriptTag);
-
-// pocket
-(!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js"));
-
-}
-</script>
-<!-- <script src="{{ asset('js/app.js') }}"></script> -->
-</body>
-</html>
+                <!-- <script src="{{ asset('js/app.js') }}"></script> -->
+              </body>
+              </html>
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -76,141 +76,115 @@
                 </ul>
               </div>
 
-              <div class="sns-area">
-                <ul class="social-button-syncer">
-                  <li class="sc-tw">
-                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="true" data-hashtags="{{config('app.name')}}">Tweet</a>
-                    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                  </li>
-
-                  <!-- Facebook -->
-                  <li class="sc-fb">
-                    <div class="fb-like" data-href="{{url()->current()}}" data-layout="box_count" data-action="like" data-show-faces="true" data-share="true"></div>
-                  </li>
-
-                  <!-- Google+ -->
-                  <li><div data-href="{{url()->current()}}" class="g-plusone" data-size="tall"></div></li>
-
-                  <!-- はてなブックマーク -->
-                  <li><a href="http://b.hatena.ne.jp/entry/{{url()->current()}}" class="hatena-bookmark-button" data-hatena-bookmark-layout="vertical-balloon" data-hatena-bookmark-lang="ja" title="このエントリーをはてなブックマークに追加"><img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border:none;" /></a></li>
-
-                  <!-- pocket -->
-                  <li><a data-save-url="{{url()->current()}}" data-pocket-label="pocket" data-pocket-count="vertical" class="pocket-btn" data-lang="en"></a></li>
-
-                  <li class="sc-li">
-                    <div class="line-it-button" data-lang="ja" data-type="share-a" data-url="{{url()->current()}}" style="display: none;"></div>
-                    <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
-                  </li>
-                </ul>
-                <div id="fb-root"></div>
+              <ul class="list-group noback">
+                @foreach($articles as $article)
+                @if($loop->iteration == 1)
+                <li class="list-group-item">
+                 <div class="padding-wrap">
+                  <div class="col-xs-3 thumbnail">
+                    <a class="thumbnail-link" href="https://lim-jp.com/archives/449">
+                      <img src="https://top.tsite.jp/static/top/sys/contents_image/036/631/669/36631669_107803.jpg" alt="【悲報】ローラ、ガチ乳首ポロリ動画をインスタグラムにアップしてしまう...">
+                    </a>
+                  </div>
+                  <div class="col-xs-9 title">
+                    <a class="title-link" href="https://lim-jp.com/archives/449">
+                     【悲報】ローラ、ガチ乳首ポロリ動画をインスタグラムにアップしてしまう...
+                   </a>
+                   <a class="description-link">
+                    @php
+                    echo mb_strimwidth("ローラのインスタグラムは「写真が素敵」「服が可愛い」と何かと話題です。最近でもニュースになった「バギー」や「ジム」の画像から、ローラの写真加工に関する情報もまとめてみました。", 0, 120, '', 'utf8');
+                    @endphp
+                  </a>
+                </div>
+                <div class="clear"></div>
+                <div class="col-xs-12 cat">
+                  <span class="cat-item">
+                    芸能
+                  </span>
+                  <span class="cat-domain">
+                    @php
+                    echo parse_url("https://lim-jp.com/archives/449", PHP_URL_HOST);
+                    @endphp
+                  </span>
+                  <a href="https://lim-jp.com/archives/449" class="link-btn">サイトへ</a>
+                </div>
+                <span class="view">{{$article->view * 2 + 32}} view</span>
               </div>
-              <div class="meta-wrap block">
-                  <p><center><a href="{{$detail->url}}" class="btn btn-primary" rel="nofollow" target="_blank">記事を読む</a></center></p>
-               </div>
-                  <ul class="list-group noback">
-                    @foreach($articles as $article)
-                    @if($loop->iteration == 1)
-                          <li class="list-group-item">
-                           <div class="padding-wrap">
-                            <div class="col-xs-3 thumbnail">
-                              <a class="thumbnail-link" href="https://lim-jp.com/archives/449">
-                                <img src="https://top.tsite.jp/static/top/sys/contents_image/036/631/669/36631669_107803.jpg" alt="【悲報】ローラ、ガチ乳首ポロリ動画をインスタグラムにアップしてしまう...">
-                              </a>
-                            </div>
-                            <div class="col-xs-9 title">
-                              <a class="title-link" href="https://lim-jp.com/archives/449">
-                               【悲報】ローラ、ガチ乳首ポロリ動画をインスタグラムにアップしてしまう...
-                             </a>
-                             <a class="description-link">
-                              @php
-                              echo mb_strimwidth("ローラのインスタグラムは「写真が素敵」「服が可愛い」と何かと話題です。最近でもニュースになった「バギー」や「ジム」の画像から、ローラの写真加工に関する情報もまとめてみました。", 0, 120, '', 'utf8');
-                              @endphp
-                            </a>
-                          </div>
-                          <div class="clear"></div>
-                          <div class="col-xs-12 cat">
-                            <span class="cat-item">
-                              芸能
-                            </span>
-                            <span class="cat-domain">
-                              @php
-                              echo parse_url("https://lim-jp.com/archives/449", PHP_URL_HOST);
-                              @endphp
-                            </span>
-                            <a href="https://lim-jp.com/archives/449" class="link-btn">サイトへ</a>
-                          </div>
-                          <span class="view">{{$article->view * 2 + 32}} view</span>
-                        </div>
-                      </li>
-                      @elseif($loop->iteration == 4)
-                          <li class="list-group-item">
-                           <div class="padding-wrap">
-                            <div class="col-xs-3 thumbnail">
-                              <a class="thumbnail-link" href="https://masakuraudo2.com/archives/2051">
-                                <img src="https://masakuraudo2.com/wp-content/uploads/2018/05/bra-2306600_640.jpg" alt="看護師・介護士のセクハラ被害の実態...">
-                              </a>
-                            </div>
-                            <div class="col-xs-9 title">
-                              <a class="title-link" href="https://masakuraudo2.com/archives/2051">
-                               看護師・介護士のセクハラ被害の実態...。実際に体を触られた例も...
-                             </a>
-                             <a class="description-link">
-                              @php
-                              echo mb_strimwidth("私も急に抱きつかれたことがあって。しかも夜中だったからめっちゃ怖かったです。。手のひらにキスされたのはほんと嫌でした。速攻手指消毒しました", 0, 120, '', 'utf8');
-                              @endphp
-                            </a>
-                          </div>
-                          <div class="clear"></div>
-                          <div class="col-xs-12 cat">
-                            <span class="cat-item">
-                              医療
-                            </span>
-                            <span class="cat-domain">
-                              @php
-                              echo parse_url("https://masakuraudo2.com/archives/2051", PHP_URL_HOST);
-                              @endphp
-                            </span>
-                            <a href="https://masakuraudo2.com/archives/2051" class="link-btn">サイトへ</a>
-                          </div>
-                          <span class="view">{{$article->view}} view</span>
-                        </div>
-                      </li>
-                      @endif
-                    <li class="list-group-item">
-                      <div class="padding-wrap">
-                        <div class="col-xs-3 thumbnail">
-                          <a class="thumbnail-link" href="{{$article->path()}}">
-                            <img src="{{$article->thumbnailPath()}}" alt="{{$article->title}}">
-                          </a>
-                        </div>
-                        <div class="col-xs-9 title">
-                          <a class="title-link" href="{{$article->path()}}">
-                           {{$article->title}}
-                         </a>
-                         <a class="description-link">
-                          @php
-                          echo mb_strimwidth($article->description, 0, 120, '', 'utf8');
-                          @endphp
-                        </a>
-                      </div>
-                      <div class="clear"></div>
-                      <div class="col-xs-12 cat">
-                        <span class="cat-item">
-                          {{$article->word->text}}
-                        </span>
-                        <span class="cat-domain">
-                          @php
-                          echo parse_url($article->url, PHP_URL_HOST);
-                          @endphp
-                        </span>
-                        <a href="{{$article->path()}}" class="link-btn">サイトへ</a>
-                      </div>
-                      <span class="view">{{$article->view}} view</span>
-                    </div>
-                  </li>
-
-                  @endforeach
-                </ul>
+            </li>
+            @elseif($loop->iteration == 4)
+            <li class="list-group-item">
+             <div class="padding-wrap">
+              <div class="col-xs-3 thumbnail">
+                <a class="thumbnail-link" href="https://masakuraudo2.com/archives/2051">
+                  <img src="https://masakuraudo2.com/wp-content/uploads/2018/05/bra-2306600_640.jpg" alt="看護師・介護士のセクハラ被害の実態...">
+                </a>
               </div>
+              <div class="col-xs-9 title">
+                <a class="title-link" href="https://masakuraudo2.com/archives/2051">
+                 看護師・介護士のセクハラ被害の実態...。実際に体を触られた例も...
+               </a>
+               <a class="description-link">
+                @php
+                echo mb_strimwidth("私も急に抱きつかれたことがあって。しかも夜中だったからめっちゃ怖かったです。。手のひらにキスされたのはほんと嫌でした。速攻手指消毒しました", 0, 120, '', 'utf8');
+                @endphp
+              </a>
             </div>
-            @endsection
+            <div class="clear"></div>
+            <div class="col-xs-12 cat">
+              <span class="cat-item">
+                医療
+              </span>
+              <span class="cat-domain">
+                @php
+                echo parse_url("https://masakuraudo2.com/archives/2051", PHP_URL_HOST);
+                @endphp
+              </span>
+              <a href="https://masakuraudo2.com/archives/2051" class="link-btn">サイトへ</a>
+            </div>
+            <span class="view">{{$article->view}} view</span>
+          </div>
+        </li>
+        @endif
+        <li class="list-group-item">
+          <div class="padding-wrap">
+            <div class="col-xs-3 thumbnail">
+              <a class="thumbnail-link" href="{{$article->path()}}">
+                <img src="{{$article->thumbnailPath()}}" alt="{{$article->title}}">
+              </a>
+            </div>
+            <div class="col-xs-9 title">
+              <a class="title-link" href="{{$article->path()}}">
+               {{$article->title}}
+             </a>
+             <a class="description-link">
+              @php
+              echo mb_strimwidth($article->description, 0, 120, '', 'utf8');
+              @endphp
+            </a>
+          </div>
+          <div class="clear"></div>
+          <div class="col-xs-12 cat">
+            <span class="cat-item">
+              {{$article->word->text}}
+            </span>
+            <span class="cat-domain">
+              @php
+              echo parse_url($article->url, PHP_URL_HOST);
+              @endphp
+            </span>
+            <a href="{{$article->path()}}" class="link-btn">サイトへ</a>
+          </div>
+          <span class="view">{{$article->view}} view</span>
+        </div>
+      </li>
+
+      @endforeach
+    </ul>
+
+    <div class="meta-wrap block">
+      <p><center><a href="{{$detail->url}}" class="btn btn-primary" rel="nofollow" target="_blank">記事を読む</a></center></p>
+    </div>
+    
+  </div>
+</div>
+@endsection
